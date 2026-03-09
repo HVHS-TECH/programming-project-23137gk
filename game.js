267592df981bf41fc8b9c2fb350dc2ad0123a3a9
23137gk <23137gk@hvhs.school.nz>
2026-03-09 13:29:43 +1300

    9/03/26 monday spell 4

--- game.js
+++ game.js
@@ -10,31 +10,33 @@
 // setup()
 /*******************************************************/
 function preload() {
-	imgUni  = loadImage('../assets/images/unicorn.png');
-	imgUnicorn2  = loadImage('../assets/images/unicorn_2.png');
+	UniFacingLeftImg  = loadImage('../assets/images/unicorn.png');
+	UniFacingRightImg  = loadImage('../assets/images/unicorn_2.png');
 }
-
 
 function setup() {
 	console.log("setup: ");
     cnv = new Canvas (windowWidth, windowHeight);
-    ball_1 = new Sprite(500, 500, 100, 'd');
-	ball_1.bounciness = 1;
-	ball_1.friction   = 0;
-	ball_1.image = (imgUni);
-	imgUni.resize(150, 150);
+
+	// Creating Uni1Sprite and linking the image
+    Uni1Sprite = new Sprite(500, 500, 100, 'd');
+	Uni1Sprite.bounciness = 1;
+	Uni1Sprite.friction   = 0;
+	Uni1Sprite.img = UniFacingLeftImg;
+	Uni1Sprite.scale = 0.2;
+
+	//Creating the ground and adding colour
+	ground = new Sprite(windowWidth, 900, 5000, 100, 'static');
+	ground.color = '#ffceec';
 }
 
-/*
+
+	/*
 	ball_2 = new Sprite(100, 300, 50, 'd');
 	ball_2.bounciness = 1;
 	ball_2.friction   = 0;
 	ball_2.image = (imgUnicorn2);
 	imgUni2.resize(100, 100);
-
-	//rectangle = new Sprite(500, 400, 100, 200);
-	//rectangle.color = 'pink';
-	//rectangle.rotationSpeed = 0;
 
 
 
@@ -43,27 +45,31 @@ function setup() {
 // draw()
 /*******************************************************/
 function draw() {
-	background('pink');
-
-	if (kb.pressing('left')) {
-    // Set sprite's velocity to the left
-			ball_1.vel.x = -5;
-	}
-
-	else if (kb.pressing ('right')) {
+	background('#fffff');
+	
+	if (kb.pressing ('right')) {
 		// Set sprite's velocity to the right
-			ball_1.vel.x = 5;
+		console.log("wheeeee")
+			Uni1Sprite.vel.x = +20;
+			Uni1Sprite.img = UniFacingRightImg
 	}
+
+	else if (kb.pressing('left')) {
+    // Set sprite's velocity to the left
+			Uni1Sprite.vel.x = -20;
+			Uni1Sprite.img = UniFacingLeftImg
+	}
+
 
 	else if (kb.released('left')) {
 		// Set sprite's velocity to zero
-		ball_1.vel.x = 0;
+		Uni1Sprite.vel.x = 0;
 
 	}
 
 	else if (kb.released('right')) {
 		// Set sprite's velocity to zero
-		ball_1.vel.x = 0;
+		Uni1Sprite.vel.x = 0;
 
 	}
 }
