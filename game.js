e77fa68e7b5b82a15c7acfcad063228a957e9062
23137gk <23137gk@hvhs.school.nz>
2026-03-10 19:25:04 +1300

    10/03/26 tuesday 7:25pm

--- game.js
+++ game.js
@@ -10,16 +10,16 @@
 // setup()
 /*******************************************************/
 function preload() {
-	UniFacingLeftImg  = loadImage('../assets/images/unicorn.png');
+	UniFacingLeftImg  = loadImage('../assets/images/unicorn.png'); // images made by me using chat gpt
 	UniFacingRightImg  = loadImage('../assets/images/unicorn_2.png');
 }
 
 function setup() {
 	console.log("setup: ");
-    cnv = new Canvas (windowWidth - 5, windowHeight- 5);
+    cnv = new Canvas (windowWidth, windowHeight);
 
 	// Creating Uni1Sprite and linking the image
-    Uni1Sprite = new Sprite(500, 690, 100, 'd');
+    Uni1Sprite = new Sprite(700, 700, 500, 'd');
 	Uni1Sprite.bounciness = 1;
 	Uni1Sprite.friction   = 0;
 	Uni1Sprite.img = UniFacingLeftImg;
@@ -30,13 +30,6 @@ function setup() {
 	ground.color = '#fcb9ca';
 }
 
-
-	/*
-	ball_2 = new Sprite(100, 300, 50, 'd');
-	ball_2.bounciness = 1;
-	ball_2.friction   = 0;
-	ball_2.image = (imgUnicorn2);
-	imgUni2.resize(100, 100);
 
 
 
