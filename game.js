ca178f1ad8e41e984d40efc3507320c8f4084990
23137gk <23137gk@hvhs.school.nz>
2026-03-26 10:59:35 +1300

    26/03/26 thursday spell 2

--- game.js
+++ game.js
@@ -29,6 +29,10 @@ function setup() {
 	console.log("setup: ");
 	cnv = new Canvas(windowWidth, windowHeight);
 
+    // walls
+    wall_left = new Sprite(5, 300, 15, 950, 'k');
+	wall_right = new Sprite(700, 700, 500, 10, 'k');
+
 	// Uni1Sprite 
 	uni1Sprite = new Sprite(700, 700, 500, 'k');
 	uni1Sprite.friction = 0;
@@ -60,7 +64,9 @@ function setup() {
 
 function collectCandy(candySprite, uni1Sprite) {
 	candySprite.remove();
-	score = score + 1;
+	for (let i = 0; i < 1; i++) {
+        score = score + 1;
+    }
 }
 
 function removeCandy(candySprite, ground) {
@@ -69,7 +75,9 @@ function removeCandy(candySprite, ground) {
 
 function hitPickle(pickleSprite, uni1Sprite) {
 	pickleSprite.remove();
-	lives = lives - 1;
+    for (let i = 0; i < 1; i++) {
+	    lives = lives - 1;
+    }
 }
 
 function removePickle(pickleSprite, ground) {
@@ -107,13 +115,11 @@ function spawnPickle() {
 
 function draw() {
 	background('#ffecf2');
-   if (gameState === "play") {
+    if (gameState === "play") {
 
-
-       // spawning
-       if(frameCount % 70 === 0) spawnCandy();
-       if(frameCount % 115 === 0) spawnPickle();
-
+         // spawning
+        if(frameCount % 70 === 0) spawnCandy();
+        if(frameCount % 115 === 0) spawnPickle();
 
        // movement
        if (kb.pressing('right')) {
@@ -128,53 +134,39 @@ function draw() {
            uni1Sprite.vel.x = 0;
        }
 
-
-      
+       // lives and score display
        textSize(30);
        fill('#ff68a7');
-       for (let i = 0; i < 1; i++) {
-           text('Score: ' + score, 50, 100);
-           text('Lives: ' + lives, width-150, 100);
-       }
-
+       text('Score: ' + score, 50, 100);
+       text('Lives: ' + lives, width-150, 100);
 
        // game over
        if (lives <= 0) {
            gameState = "gameOver";
            uni1Sprite.vel.x = 0;
-
-
            lollyGroup.removeAll();
            pickleGroup.removeAll();
        }
-
-
-   } else if (gameState === "gameOver") {
-       textAlign(CENTER);
-       textSize(70);
-       fill('#ff8f8f');
-       text('GAME OVER', width/2, height/2);
-
-
-       textSize(30);
-       text('Press SPACE to restart', width/2, height/2 + 50);
+       // if gamestate equals game over, game ends, press space to restart
+       else if (gameState === "gameOver") {
+            textAlign(CENTER);
+            textSize(70);
+            fill('#ff8f8f');
+            text('GAME OVER', width/2, height/2);
+            textSize(30);
+            text('Press SPACE to restart', width/2, height/2 + 50);
+       }
 
 
        // restart game
        if (kb.presses('space')) {
            score = 0;
            lives = 3;
-
-
            lollyGroup.removeAll();
            pickleGroup.removeAll();
-
-
            uni1Sprite.x = width/2;
            uni1Sprite.y = 700;
            uni1Sprite.vel.x = 0;
-
-
            gameState = "play";
        }
    }
